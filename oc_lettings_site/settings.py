a93848c04f920152d4799f07400e0e983718b267
Kassim <kassimbouzoubaa05@gmail.com>
2024-08-09 03:51:13 +0200

    Ajout du formattage avec black dans la pipeline

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -28,7 +28,12 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver",'python-oc-lettings-fr-sgwn.onrender.com']
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "testserver",
+    "python-oc-lettings-fr-sgwn.onrender.com",
+]
 
 
 # Application definition
